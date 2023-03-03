84a33c6fdb2a797388386de5e8a9d5efc785e198
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-03-04 01:21:09 +0800

    add

---
+++ perl_script/fasta file script/clean_fasta_bp.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+use Getopt::Std;
+getopt("fr");
+$file1=0;
+open FILE,$opt_f;
+while(<FILE>){
+if (/>/){
+next();}
+else{
+chomp;
+$length=length $_;
+$file1+=$length;
+}
+}
+close FILE;
+
+open SEQ,$opt_r;
+$file2=0;
+while(<SEQ>){
+if (/>/){
+next();}
+else{
+chomp;
+$length=length $_;
+$file2+=$length;
+}
+}
+$datasetsize=$file1+$file2;
+
+print "$file1\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
