b778a1c78f737b1e45a8ffb8cd6a67644fec6bf2
ianyen77 <96662135+ianyen77@users.noreply.github.com>
2023-03-18 19:33:02 +0800

    new scripts

---
+++ perl_script/MGE_VF_blast.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+#Author:2023/03/04
+#This is script for 1.blast ARC_orf with MGE_DB 2.blast ARC_ORF with VF_DB
+#----------------------------------
+#
+use Getopt::Std;
+getopt("imvb");
+
+unless (@ARGV && $opt_m && $opt_i && $opt_v &&$opt_b ){
+    print "-i iutput ARC_ORF directory(abs_path)\n";
+    print "-m output MGE diamond blast directory(abs_path)\n";
+    print "-v output VF diamond blast directory(abs_path)\n";
+    print "-ARGV are input orf.nucl file(abs_path)\n";
+    print "-b output BacMet diamond blast directoury(abs_path)\n";
+    die();
+    }
+mkdir $opt_b;
+mkdir $opt_m;
+mkdir $opt_v;
+for ($x=0; $x<@ARGV; $x++){
+	if ($ARGV[$x] =~ /(\S+\/)(\S+)(.nucl)/){
+	$filename=$2;
+	}
+	$orf=$opt_i.$filename.".nucl";
+	$MGE_diamond=$opt_m.$filename."MGEs.dmnd";
+	$VF_diamond=$opt_v.$filename."VF.dmnd";
+	$bac_diamond=$opt_b.$filename."BacMet.dmnd";
+	system("diamond blastx -d /media/sf_sf/DB/Diamond/DB/MGEs_2018nature_commu.dmnd -q $orf -p 16 --id 70 -p 16 -e 1e-10 -f 6 -k 1 --query-cover 50 -o $MGE_diamond");
+	system("diamond blastx -d /media/sf_sf/DB/Diamond/DB/VFDB_coredataset.dmnd -q $orf -p 16 --id 70 -p 16 -e 1e-10 -f 6 -k 1 --query-cover 50 -o $VF_diamond");
+	system("diamond blastx -d /media/sf_sf/DB/Diamond/DB/bacmet_experiment.dmnd -q $orf -p 16 --id 70 -p 16 -e 1e-10 -f 6 -k 1 --query-cover 50 -o $bac_diamond");
+	}
--- perl_script/clean_fasta_bp.pl
+++ perl_script/clean_fasta_bp.pl
@@ -1,10 +1,21 @@
 #!/usr/bin/perl -w
+#opt_r=input cleanread directory(abs.path)
 use Getopt::Std;
-getopt("fr");
+getopt("r");
+print "sample\treadsnumber\tbp\n";
+for ($x=0; $x<@ARGV; $x++){
+	if ($ARGV[$x] =~ /(\S+\/)(\S+)(_1.fa)/){
+	$filename=$2;
+	chomp $filename;
+	}
+$clean1=$opt_r.$filename."_1.fa";
+$clean2=$opt_r.$filename."_2.fa";
 $file1=0;
-open FILE,$opt_f;
+$count1=0;
+open FILE,$clean1;
 while(<FILE>){
 if (/>/){
+$count1++;
 next();}
 else{
 chomp;
@@ -14,10 +25,12 @@ $file1+=$length;
 }
 close FILE;
 
-open SEQ,$opt_r;
+open SEQ,$clean2;
 $file2=0;
+$count2=0;
 while(<SEQ>){
 if (/>/){
+$count2++;
 next();}
 else{
 chomp;
@@ -27,10 +40,10 @@ $file2+=$length;
 }
 close SEQ;
 
-$datasetsize=$file1+$file2;
-
-print "$datasetsize\n";
-
+$file_f=$filename."_1";
+$file_r=$filename."_2";
+print "$file_f\t$count1\t$file1\n$file_r\t$count2\t$file2\n";	
+}
 
 
 
